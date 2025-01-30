86553065f14e12a846e9309fb5d093df2829d231
Rakesh Kumar <acousticrakesh0827@gmail.com>
2025-01-30 23:05:13 +0530

    Shop UI using angular - 9

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa48122a1a9ad0b7410b8fba576059161e195587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb21cdd703418b714470bee8bb01dd358b8ad0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
